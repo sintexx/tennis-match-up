2c7aea03de60df2ee0366220bb0e115c61e842ac
Niels Knoll <niels.knoll@codecentric.de>
2023-10-20 21:53:12 +0200

    add player view

--- assets/md.transition-legacy-3a9ed597.js
+++ assets/md.transition-legacy-3a9ed597.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ce8d2846.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
+System.register(["./index-legacy-89495d3d.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- assets/status-tap-legacy-270a0a8e.js
+++ assets/status-tap-legacy-270a0a8e.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ce8d2846.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-89495d3d.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
