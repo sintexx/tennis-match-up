a46d9eb8b0594d98664c52ecfcd0f32221c40bb9
Niels Knoll <niels.knoll@codecentric.de>
2023-10-21 14:48:32 +0200

    add input checks

--- assets/ios.transition-legacy-416aed83.js
+++ assets/ios.transition-legacy-416aed83.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5f0b5448.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.o}],execute:function(){
+System.register(["./index-legacy-9a640f68.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- assets/md.transition-legacy-d4435714.js
+++ assets/md.transition-legacy-d4435714.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5f0b5448.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
+System.register(["./index-legacy-9a640f68.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
